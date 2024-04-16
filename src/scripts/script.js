cfc241f065825cdc5e672ddf2f8de1d74db66eea
Ginaldo-Silva <138523497+Ginaldo-Silva@users.noreply.github.com>
2024-04-16 16:50:08 -0300

    .

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,24 @@
 const burger = document.getElementById('burger');
 const navbar = document.getElementById('navbar');
 
-if(burger.checked) {
-    console.log("!!!");
-} else {
-    console.log("???");
+burger.addEventListener('change', function() {
+    if (burger.checked) {
+        navbar.classList.add('active-navbar');
+    } else {
+        navbar.classList.remove('active-navbar');
+    }
+});
+
+function onClickLink() {
+    burger.checked = false;
+    navbar.classList.remove('active-navbar');
 }
+
+document.addEventListener('mousedown', (event) => {
+    if(burger.checked) {
+        if(!navbar.contains(event.target) && !event.target.closest('.burger')) {
+            burger.checked = false;
+            navbar.classList.remove('active-navbar');
+        }
+    }
+});
