a3e713caf9f0826d310da4aae6462e099c166432
Ginaldo-Silva <138523497+Ginaldo-Silva@users.noreply.github.com>
2024-04-16 16:37:41 -0300

    Header

---
+++ src/scripts/script.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById('burger');
+const navbar = document.getElementById('navbar');
+
+if(burger.checked) {
+    console.log("!!!");
+} else {
+    console.log("???");
+}
